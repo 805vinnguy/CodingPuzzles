7b9b4f7843975793707d2f53031a9195874fd2b1
Vinh Nguyen <805vinnguy@gmail.com>
2019-04-20 13:11:36 -0700

    better solution product_array(), uses div

--- ProductArray/productarray.py
+++ ProductArray/productarray.py
@@ -1,6 +1,7 @@
 # Product Array
 # Author: Vinh Nguyen
 
+# O(2n-1)
 def product_array_no_div(input_arr):
     # [1, 2, 3, 4, 5]
     # [1, 1, 1, 1, 1]
@@ -16,6 +17,7 @@ def product_array_no_div(input_arr):
         i += 1
     return output_arr
 
+# O(2n)
 def product_array_div(input_arr):
     size = len(input_arr)
     tot_prod = 1
@@ -24,4 +26,19 @@ def product_array_div(input_arr):
     output_arr = [tot_prod] * size
     for i in range(size):
         output_arr[i] /= input_arr[i]
+    return output_arr
+
+# O(2n-2)
+def product_array(input_arr):
+    # [1, 2, 3, 4, 5]
+    # [(60/1)*2, (40/2)*3, (30/3)*4, (24/4)*5, 24=(120/5)*1]
+    size = len(input_arr)
+    output_arr = [1] * size
+    for i in range(size - 1):
+        output_arr[-1] *= input_arr[i]
+    for i in range(1, size):
+        # start from the end
+        iOut = size - 1 - i
+        iPrev = iOut + 1
+        output_arr[iOut] = (output_arr[iPrev] / input_arr[iOut]) * input_arr[iPrev]
     return output_arr
--- ProductArray/tests.py
+++ ProductArray/tests.py
@@ -20,13 +20,25 @@ class TestProductArray(unittest.TestCase):
     def test_1_div(self):
         input_arr = [1, 2, 3, 4, 5]
         expect = [120, 60, 40, 30, 24]
-        output_arr = productarray.product_array_no_div(input_arr)
+        output_arr = productarray.product_array_div(input_arr)
         self.assertEqual(output_arr, expect)
 
     def test_2_div(self):
         input_arr = [3, 2, 1]
         expect = [2, 3, 6]
-        output_arr = productarray.product_array_no_div(input_arr)
+        output_arr = productarray.product_array_div(input_arr)
+        self.assertEqual(output_arr, expect)
+
+    def test_1(self):
+        input_arr = [1, 2, 3, 4, 5]
+        expect = [120, 60, 40, 30, 24]
+        output_arr = productarray.product_array(input_arr)
+        self.assertEqual(output_arr, expect)
+
+    def test_2(self):
+        input_arr = [3, 2, 1]
+        expect = [2, 3, 6]
+        output_arr = productarray.product_array(input_arr)
         self.assertEqual(output_arr, expect)
 
 if __name__ == "__main__":
